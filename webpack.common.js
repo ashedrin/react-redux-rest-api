b5013f92c7d9502abdc49870c2c22b10c07e5038
Alex Shchedrin <ashchedrin@klika-tech.com>
2020-02-18 17:25:46 +0300

    Separate webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,17 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    contentBase: './dist',
-  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ title: 'Production' }),
+  ],
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
